5ea626f16eb602e8f856a919f49774f23d139d64
Sarthak <54149390+sarthaknimbalkar@users.noreply.github.com>
2024-08-26 02:14:59 +0530

    Update and rename r to resource_tracker.sh

---
+++ Day 7/resource_tracker.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+######################################################
+#
+# Author: Sarthak
+# Date: 26th Aug, 2024
+#
+# Description: This script generates an AWS usage report,
+# listing S3 buckets, EC2 instances, Lambda functions,
+# and IAM users.
+#
+######################################################
+
+# Enable debug mode
+set -euo pipefail
+
+# Function to list S3 buckets
+list_s3_buckets() {
+    echo "Listing S3 buckets..."
+    aws s3 ls
+}
+
+# Function to list EC2 instances
+list_ec2_instances() {
+    echo "Listing EC2 instances..."
+    aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text
+}
+
+# Function to list Lambda functions
+list_lambda_functions() {
+    echo "Listing Lambda functions..."
+    aws lambda list-functions --query 'Functions[*].FunctionName' --output text
+}
+
+# Function to list IAM users
+list_iam_users() {
+    echo "Listing IAM users..."
+    aws iam list-users --query 'Users[*].UserName' --output text
+}
+
+# Execute functions
+list_s3_buckets
+list_ec2_instances
+list_lambda_functions
+list_iam_users
